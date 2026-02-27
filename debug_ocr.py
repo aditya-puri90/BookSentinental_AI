7d3c016887c703162769d119248f7f45821f3aa9
aditya puri <puriaditya90@gmail.com>
2026-02-27 12:43:56 +0530

    Add debug_ocr.py script

---
+++ debug_ocr.py
@@ -0,0 +1,17 @@
+﻿import main
+import cv2
+
+def run_debug():
+    monitor = main.SmartLibraryMonitor()
+    cap = cv2.VideoCapture('book.mp4')
+    c = 0
+    while cap.isOpened() and c < 300:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        monitor.process_frame(frame)
+        c += 1
+    cap.release()
+
+if __name__ == "__main__":
+    run_debug()
